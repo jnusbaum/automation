3bcbab5d339e6b09647eff78aaeab923bbc33415
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-10-25 15:47:27 -0600

    data model

--- heating/models.py
+++ heating/models.py
@@ -1,3 +1,22 @@
 from django.db import models
 
 # Create your models here.
+class Sensor(models.Model):
+    name = models.CharField(max_length=64, primary_key=True)
+    type = models.CharField(max_length=8, choices=[('TEMP', 'Temperature'), ('POS', 'Position'), ('ONOFF', 'On/Off')])
+    address = models.CharField(max_length=128, blank=True)
+    description = models.CharField(max_length=512)
+
+    class Meta:
+        verbose_name = "Sensor"
+        verbose_name_plural = "Sensors"
+
+class SensorData(models.Model):
+    name = models.ForeignKey('Sensor', on_delete=models.CASCADE)
+    timestamp = models.DateTimeField(auto_now_add=True)
+    value_real = models.DecimalField(max_digits=10, decimal_places=2, null=True)
+    value_bool = models.BooleanField(null=True)
+
+    class Meta:
+        verbose_name = "SensorData"
+        verbose_name_plural = "SensorData"
