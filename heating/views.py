98055e57512e879a31ab5be511576de607b815c6
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-10-26 18:43:35 -0600

    view stubs

--- heating/views.py
+++ heating/views.py
@@ -1,6 +1,5 @@
-from django.shortcuts import render
 from django.http import HttpResponse
-
+from heating.models import Sensor, SensorData
 
 # Create your views here.
 def api_index(request):
@@ -10,32 +9,33 @@ def api_index(request):
 
 def api_sensors(request):
     if request.method == 'POST':
-    # if POST add new sensor
+        # if POST add new sensor
+        s = Sensor(name=request.POST['name'], type=)
         pass
     else:
-    # if GET return list of sensors
-        pass
+        # if GET return list of sensors
+        sensors = Sensor.objects.all()
     return HttpResponse("sensors")
 
 
 def api_sensor(request, sensor_name):
     if request.method == 'PATCH':
-    # if PATCH add data for sensor
+        # if PATCH add data for sensor
         pass
     elif request.method == 'DELETE':
-    # if DELETE get data for sensor
+        # if DELETE get data for sensor
         pass
     else:
-    # if GET get sensor mets data
+        # if GET get sensor meta data
         pass
     return HttpResponse("sensor")
 
 
 def api_sensor_data(request, sensor_name):
     if request.method == 'POST':
-    # if POST add data for sensor
+        # if POST add data for sensor
         pass
     else:
-    # if GET get data for sensor
+        # if GET get data for sensor
         pass
     return HttpResponse("sensor_data")
