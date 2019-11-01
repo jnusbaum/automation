39e084f697d149b6cfdc5a10081b25439c6481ee
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-11-01 16:43:01 -0600

    more api work

--- heating/views.py
+++ heating/views.py
@@ -1,6 +1,7 @@
 from django.http import HttpResponse, JsonResponse
 from django.utils import timezone
 from django.core import serializers
+from django.shortcuts import get_object_or_404
 from heating.models import Sensor, SensorData
 
 # Create your views here.
@@ -25,14 +26,33 @@ def api_sensors(request):
 def api_sensor(request, sensor_name):
     if request.method == 'PATCH':
         # if PATCH add data for sensor
-        pass
+        sensor = get_object_or_404(Sensor, pk=sensor_name)
+        # can't change pk (name)
+        try:
+            sensor.type = request.POST['type']
+        except KeyError:
+            pass
+        try:
+            sensor.address = request.POST['address']
+        except KeyError:
+            pass
+        try:
+            sensor.description = request.POST['description']
+        except KeyError:
+            pass
+        sensor.save()
+        return HttpResponse(status=204)
     elif request.method == 'DELETE':
         # if DELETE delete sensor
-        pass
+        sensor = get_object_or_404(Sensor, pk=sensor_name)
+        sensor.delete()
+        return HttpResponse(status=204)
     else:
         # if GET get sensor meta data
-        pass
-    return HttpResponse("sensor")
+        sensor = get_object_or_404(Sensor, pk=sensor_name)
+        data = serializers.serialize('json', sensor)
+        return JsonResponse(data)
+
 
 bool_values = {
     'on': True,
@@ -51,17 +71,13 @@ def api_sensor_data(request, sensor_name):
         print("in api_sensor_data")
         # if POST add data for sensor
         timestamp = request.POST.get('timestamp', timezone.now())
-        print(timestamp)
         value_real = request.POST.get('value-real')
-        print(value_real)
         if value_real:
             value_real = float(value_real)
             print(value_real)
         value_bool = request.POST.get('value-bool')
-        print(value_bool)
         if value_bool:
             value_bool = bool_values[value_bool]
-            print(value_bool)
         s = Sensor.objects.get(name=sensor_name)
         sd = SensorData(sensor=s, timestamp=timestamp, value_real=value_real, value_bool=value_bool)
         sd.save()
