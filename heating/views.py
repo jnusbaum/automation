2bd28d6d6b802e9d151dca254dc0edcb0298babd
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-12-12 18:09:45 -0700

    heating dashboard alpha with colors

--- heating/views.py
+++ heating/views.py
@@ -1,10 +1,11 @@
 from django.http import HttpResponse
 from django.shortcuts import render
 import requests
+from decimal import *
 
 host = 'http://192.168.0.134'
 
-# Create your views here.
+
 def index(request):
     # fake samples
     # samples = [
@@ -16,7 +17,7 @@ def index(request):
     # get samples from data server
     # first get sensors
     r = requests.get(f'{host}/sensors')
-    if (requests.codes.ok != r.status_code):
+    if requests.codes.ok != r.status_code:
         # error
         return HttpResponse(status_code=r.status_code)
     data = r.json()
@@ -24,10 +25,23 @@ def index(request):
     samples = []
     for sensor in sensors:
         r = requests.get(f"{host}/sensors/{sensor['id']}/data")
-        if (requests.codes.ok != r.status_code):
+        if requests.codes.ok != r.status_code:
             # error
             return HttpResponse(status_code=r.status_code)
         data = r.json()
         for sample in data['data']:
-            samples.append({'name': sensor['id'], 'timestamp': sample['attributes']['timestamp'], 'value': sample['attributes']['value_real']})
+            dval = Decimal(sample['attributes']['value_real'])
+            if dval < 70:
+                dclass = 'cold'
+            elif dval < 90:
+                dclass = 'cool'
+            elif dval < 110:
+                dclass = 'luke-warm'
+            elif dval < 130:
+                dclass = 'warm'
+            elif dval < 150:
+                dclass = 'verywarm'
+            else:
+                dclass = 'hot'
+            samples.append({'name': sensor['id'], 'timestamp': sample['attributes']['timestamp'], 'value': sample['attributes']['value_real'], 'dclass': dclass})
     return render(request, 'heating/heating-dashboard.html', {'samples': samples})
