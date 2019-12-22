9cfb7ca163bb708ad58e1a83fcfa14b85c04645c
Jim Nusbaum <jnusbaum@supraluminalsoftware.com>
2019-12-21 21:52:35 -0700

    static files

--- automation/urls.py
+++ automation/urls.py
@@ -17,6 +17,6 @@ from django.contrib import admin
 from django.urls import path, re_path, include
 
 urlpatterns = [
-    re_path('admin/', admin.site.urls),
-    re_path('dashboard/', include('heating.urls')),
+    re_path('automation/admin/', admin.site.urls),
+    re_path('automation/dashboard/', include('heating.urls')),
 ]
