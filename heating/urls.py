993cd6c75fd67ae080def9165f7fec325ccb3bb9
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2019-12-22 21:23:25 -0700

    comparison charts

--- heating/urls.py
+++ heating/urls.py
@@ -4,5 +4,6 @@ from . import views
 
 urlpatterns = [
     path('heating/', views.index, name='index'),
+    path('heating/all/', views.view_all, name='view_all'),
     path('heating/<zone_name>/', views.zone, name='zone'),
 ]
--- heating/views.py
+++ heating/views.py
@@ -6,6 +6,8 @@ from datetime import datetime
 
 host = 'http://192.168.0.134'
 
+zones = ('MBR', 'MBATH', 'LIBRARY', 'KITCHEN', 'GARAGE', 'FAMILY', 'OFFICE', 'EXERCISE', 'GUEST', 'VALVE', 'BOILER')
+
 def str_to_datetime(ans):
     if ans:
         d = datetime.strptime(ans, "%Y-%m-%d-%H-%M-%S")
@@ -15,13 +17,6 @@ def str_to_datetime(ans):
 
 
 def index(request):
-    # fake samples
-    # samples = [
-    #     {'name': 'MBR-IN', 'timestamp': '2019-12-12-14-22-34', 'value': '142.45'},
-    #     {'name': 'MBR-OUT', 'timestamp': '2019-12-12-14-22-34', 'value': '102.00'},
-    #     {'name': 'OFFICE-IN', 'timestamp': '2019-12-12-14-22-34', 'value': '94.50'},
-    #     {'name': 'OFFICE-OUT', 'timestamp': '2019-12-12-14-22-34', 'value': '92.67'},
-    # ]
     # get samples from data server
     # first get sensors
     r = requests.get(f'{host}/sensors')
@@ -52,7 +47,10 @@ def index(request):
                 dclass = 'very-warm'
             else:
                 dclass = 'hot'
-            samples[sensor['id'].replace('-', '_')] = {'name': sensor['id'], 'timestamp': sample['attributes']['timestamp'], 'value': sample['attributes']['value_real'], 'dclass': dclass}
+            samples[sensor['id'].replace('-', '_')] = {'name': sensor['id'],
+                                                       'timestamp': sample['attributes']['timestamp'],
+                                                       'value': sample['attributes']['value_real'],
+                                                       'dclass': dclass}
     return render(request, 'heating/heating-dashboard.html', {'samples': samples})
 
 
@@ -80,7 +78,6 @@ def clean_data(sdata):
         prev_out = outval
         outdata.append((timestamp, inval, outval))
     return outdata, badin, badout
-
 
 
 def zone(request, zone_name):
@@ -111,3 +108,36 @@ def zone(request, zone_name):
     samples = {'data': sdata, 'badin': badin, 'badout': badout, 'zone': zone_name }
 
     return render(request, 'heating/heating-zone.html', {'samples': samples})
+
+
+def view_all(request):
+    # get data for zone
+    # input, will return latest value
+    params = {'datapts': 9000}
+    samples = {}
+    for zone_name in zones:
+        if zone_name == 'VALVE':
+            r = requests.get(f'{host}/sensors/{zone_name}-INSYS/data', params=params)
+        else:
+            r = requests.get(f'{host}/sensors/{zone_name}-IN/data', params=params)
+        if requests.codes.ok != r.status_code:
+            # error
+            return HttpResponse(status_code=r.status_code)
+        data = r.json()
+        invals = data
+        # output
+        r = requests.get(f'{host}/sensors/{zone_name}-OUT/data', params=params)
+        if requests.codes.ok != r.status_code:
+            # error
+            return HttpResponse(status_code=r.status_code)
+        data = r.json()
+        outvals = data
+        # join data
+        sdata = [(str_to_datetime(invals['data'][i]['attributes']['timestamp']),
+                  Decimal(invals['data'][i]['attributes']['value_real']),
+                  Decimal(outvals['data'][i]['attributes']['value_real'])) for i in range(0, invals['count'])]
+        sdata, badin, badout = clean_data(sdata)
+        samples[zone_name] = sdata
+
+    return render(request, 'heating/heating-all.html', {'samples': samples})
+
