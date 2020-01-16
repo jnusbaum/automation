8b2b438a57ad9da2b15bc3579f392e58c4b4dd51
Jim Nusbaum <jim.nusbaum@vitalityindex.com>
2020-01-16 15:23:53 -0700

    alpha - port completely to django/sqlite

--- heating/views.py
+++ heating/views.py
@@ -1,68 +1,30 @@
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotFound
 from django.shortcuts import render
-import requests
-from decimal import *
-from datetime import datetime
-
 from django.conf import settings
-
-
-def str_to_datetime(ans):
-    if ans:
-        d = datetime.strptime(ans, "%Y-%m-%d-%H-%M-%S")
-        return d
-    return None
-
-
-def datetime_to_str(ans):
-    if ans:
-        d = ans.strftime("%Y-%m-%d-%H-%M-%S")
-        return d
-    return None
+from heating.models import *
 
 
 def dashboard(request):
-    # get samples from data server
-    r = requests.get(f'{settings.DATASERVER_HOST}/zones')
-    if requests.codes.ok != r.status_code:
-        # error
-        return HttpResponse(status=r.status_code)
-    data = r.json()
-    zones = data['data']
-    zones.sort(key=lambda x: x['id'])
-
+    zones = Zone.objects.all().order_by('name')
     return render(request, 'heating/heating-dashboard.html', {'host': settings.DATASERVER_HOST, 'zones': zones})
 
 
 def zone(request, zone_name):
-    r = requests.get(f'{settings.DATASERVER_HOST}/zones')
-    if requests.codes.ok != r.status_code:
-        # error
-        return HttpResponse(status=r.status_code)
-    data = r.json()
-    zones = data['data']
-    zones.sort(key=lambda x: x['id'])
+    zones = Zone.objects.all().order_by('name')
     return render(request, 'heating/heating-zone.html', {'host': settings.DATASERVER_HOST, 'zone': zone_name, 'zones': zones})
 
 
-def all(request):
+def all_zones(request):
     # get data for all zones
     datapts = request.GET.get('datapts', '100')
-    # look for zone names
-    r = requests.get(f'{settings.DATASERVER_HOST}/zones')
-    if requests.codes.ok != r.status_code:
-        # error
-        return HttpResponse(status=r.status_code)
-    data = r.json()
-    zones = data['data']
-    zones.sort(key=lambda x: x['id'])
+    zones = Zone.objects.all().order_by('name')
     dzones = []
     if 'ALL' in request.GET:
         dzones = zones
     else:
-        for zone in zones:
-            if zone['id'] in request.GET:
-                dzones.append(zone)
+        for z in zones:
+            if z.name in request.GET:
+                dzones.append(z)
 
     return render(request, 'heating/heating-all.html', {'host': settings.DATASERVER_HOST,
                                                         'datapts': datapts,
@@ -73,27 +35,17 @@ def all(request):
 def overlay(request):
     # get data for all zones
     datapts = request.GET.get('datapts', '100')
-    # look for zone names
-    r = requests.get(f'{settings.DATASERVER_HOST}/zones')
-    if requests.codes.ok != r.status_code:
-        # error
-        return HttpResponse(status=r.status_code)
-    data = r.json()
-    zones = data['data']
-    zones.sort(key=lambda x: x['id'])
+    zones = Zone.objects.all().order_by('name')
     dzones = []
     if 'ALL' in request.GET:
         dzones = zones
     else:
-        for zone in zones:
-            if zone['id'] in request.GET:
-                dzones.append(zone)
+        for z in zones:
+            if z.name in request.GET:
+                dzones.append(z)
     osensors = []
-    for zone in dzones:
-        r = requests.get(f"{settings.DATASERVER_HOST}/sensors/zone/{zone['id']}")
-        data = r.json()
-        sensors = data['data']
-        for sensor in sensors:
+    for z in dzones:
+        for sensor in z.sensor_set.all():
             osensors.append(sensor)
     return render(request, 'heating/heating-overlay.html', {'host': settings.DATASERVER_HOST,
                                                             'datapts': datapts,
