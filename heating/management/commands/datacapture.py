b421d76591f6b21648735800202f628ff15afab4
Jim Nusbaum <jim.nusbaum@vitalityindex.com>
2020-01-28 15:54:36 -0700

    moved data capture into django

---
+++ heating/management/commands/datacapture.py
@@ -0,0 +1,46 @@
+from django.core.management.base import BaseCommand
+from heating.models import *
+import os
+import logging
+from datetime import datetime
+import json
+import paho.mqtt.client as mqtt
+from django.conf import settings
+
+
+# The callback for when the client receives a CONNACK response from the server.
+def on_connect(client, userdata, flags, rc):
+    # Subscribing in on_connect() means that if we lose the connection and
+    # reconnect then subscriptions will be renewed.
+    client.subscribe(settings.TOPIC)
+
+
+# The callback for when a PUBLISH message is received from the server.
+def on_message(client, userdata, msg):
+    # save data to db
+    # zone payload is JSON object containing sensor value
+    #         {
+    #             'sensor': 'BOILER-IN',
+    #             'timestamp': '2020-01-11T14:33:10.772357',
+    #             'value': 142.3
+    #         }
+    payload = json.loads(msg.payload)
+    fsname = payload['sensor']
+    timestamp = datetime.fromisoformat(payload['timestamp'])
+    value = payload['value']
+    s = SensorData(sensor_id=fsname, timestamp=timestamp, value=value, original_value=value)
+    s.save()
+
+
+class Command(BaseCommand):
+    help = 'capture sensor data'
+
+    def handle(self, *args, **options):
+        client = mqtt.Client()
+        client.on_connect = on_connect
+        client.on_message = on_message
+        client.connect(settings.MQTTHOST)
+
+        # Blocking call that processes network traffic, dispatches callbacks and
+        # handles reconnecting.
+        client.loop_forever()
