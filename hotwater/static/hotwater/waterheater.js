619528ee1ea69ada3ded82ddbe285267e72d5227
Jim Nusbaum <jnusbaum@supraluminalsoftware.com>
2021-02-27 12:47:46 -0700

    refactoring

--- hotwater/static/hotwater/waterheater.js
+++ hotwater/static/hotwater/waterheater.js
@@ -1,8 +1,19 @@
 
 
+function __heaterDataCallback(adata, heater, func) {
+    func.call(heater, adata);
+}
 
-function WaterHeater(name, in_div, out_div, burn_div, chart_div) {
+function __heaterCallback(heater, func) {
+    func.call(heater);
+}
+
+
+function WaterHeater(name, in_div, out_div, burn_div, chart_div, url, period) {
     this.name = name;
+    this.url = url;
+    this.period = period;
+
     this.chartConfig = {
         type: 'line',
         data: {
@@ -110,42 +121,143 @@ function WaterHeater(name, in_div, out_div, burn_div, chart_div) {
 
     this.offset = new Date().getTimezoneOffset() * 60 * 1000;
 
-    this.updateData = function(adata, shift) {
+    // max data points to display in chart
+    this.maxPoints = 9000;
 
+    // dataset indices
+    this.inIndex = 0;
+    this.outIndex = 1;
+    this.burnIndex = 2;
+
+    // last ts loaded
+    this.lastLoaded;
+
+    this.updateData = function(adata) {
+        let sincount = adata['data']['sensor_in']['count'];
         let sindata = adata['data']['sensor_in']['data']
-        let soutdata = adata['data']['sensor_out']['data']
-        let sburndata = adata['data']['sensor_burn']['data']
-        let numpts = adata['data']['sensor_in']['count'];
-        if (numpts > 0) {
+        if (sincount > 0) {
+            // data comes in latest first
             this.gaugeIn.value = sindata[0]['attributes']['value'];
-            this.gaugeOut.value = soutdata[0]['attributes']['value'];
-            this.gaugeBurn.value = sburndata[0]['attributes']['value'];
-            // reverse load
-            for (let i = numpts - 1; i >= 0; i--) {
-                this.lineChart.data.datasets[0].data.push({
-                    t: sindata[i]['attributes']['timestamp'] - this.offset,
-                    y: sindata[i]['attributes']['value']
-                });
-                this.lineChart.data.datasets[1].data.push({
-                    t: soutdata[i]['attributes']['timestamp'] - this.offset,
-                    y: soutdata[i]['attributes']['value']
-                });
-                this.lineChart.data.datasets[2].data.push({
-                    t: sburndata[i]['attributes']['timestamp'] - this.offset,
-                    y: sburndata[i]['attributes']['value']
-                });
-            }
-            if (shift) {
-                // remove extras
-                let l = this.lineChart.data.datasets[0].data.length;
-                for (let i = 0; i < l - 9000; i++) {
-                    this.lineChart.data.datasets[0].data.shift();
-                    this.lineChart.data.datasets[1].data.shift();
-                    this.lineChart.data.datasets[2].data.shift();
+            let sinLen = 0;
+            if (this.lineChart.data.datasets[this.inIndex].data.length > 0) {
+                // already have data
+                for (let i = sincount - 1; i >= 0; i--) {
+                    sinLen = this.lineChart.data.datasets[this.inIndex].data.push({
+                        t: sindata[i]['attributes']['timestamp'] - this.offset,
+                        y: sindata[i]['attributes']['value']
+                    });
+                    if (sinLen > this.maxPoints) {
+                        // remove extra
+                        this.lineChart.data.datasets[this.inIndex].data.shift();
+                    }
                 }
+            } else {
+                // no data
+                let sinLen = 0;
+                for (let i = 0; i < sincount; i++) {
+                    sinLen = this.lineChart.data.datasets[this.inIndex].data.unshift({
+                        t: sindata[i]['attributes']['timestamp'] - this.offset,
+                        y: sindata[i]['attributes']['value']
+                    });
+                }
+                // guaranteed not to have more than maxPoints in the incoming data\
+                // so no need to remove extras
+            }
+        }
+
+        let soutcount = adata['data']['sensor_out']['count']
+        let soutdata = adata['data']['sensor_out']['data']
+        if (soutcount > 0) {
+            // data comes in latest first
+            this.gaugeOut.value = soutdata[0]['attributes']['value'];
+            let soutLen = 0;
+            if (this.lineChart.data.datasets[this.outIndex].data.length > 0) {
+                // already have data
+                for (let i = soutcount - 1; i >= 0; i--) {
+                    soutLen = this.lineChart.data.datasets[this.outIndex].data.push({
+                        t: soutdata[i]['attributes']['timestamp'] - this.offset,
+                        y: soutdata[i]['attributes']['value']
+                    });
+                    if (soutLen > this.maxPoints) {
+                        // remove extra
+                        this.lineChart.data.datasets[this.outIndex].data.shift();
+                    }
+                }
+            } else {
+                // no data
+                let soutLen = 0;
+                for (let i = 0; i < soutcount; i++) {
+                    soutLen = this.lineChart.data.datasets[this.outIndex].data.unshift({
+                        t: soutdata[i]['attributes']['timestamp'] - this.offset,
+                        y: soutdata[i]['attributes']['value']
+                    });
+                }
+                // guaranteed not to have more than maxPoints in the incoming data\
+                // so no need to remove extras
+            }
+        }
+
+        let sburncount = adata['data']['sensor_burn']['count']
+        let sburndata = adata['data']['sensor_burn']['data']
+        if (sburncount > 0) {
+            // data comes in latest first
+            this.gaugeBurn.value = sburndata[0]['attributes']['value'];
+            let sburnLen = 0;
+            if (this.lineChart.data.datasets[this.burnIndex].data.length > 0) {
+                // already have data
+                for (let i = sburncount - 1; i >= 0; i--) {
+                    sburnLen = this.lineChart.data.datasets[this.burnIndex].data.push({
+                        t: sburndata[i]['attributes']['timestamp'] - this.offset,
+                        y: sburndata[i]['attributes']['value']
+                    });
+                    if (sburnLen > this.maxPoints) {
+                        // remove extra
+                        this.lineChart.data.datasets[this.burnIndex].data.shift();
+                    }
+                }
+            } else {
+                // no data
+                let sburnLen = 0;
+                for (let i = 0; i < sburncount; i++) {
+                    sburnLen = this.lineChart.data.datasets[this.burnIndex].data.unshift({
+                        t: sburndata[i]['attributes']['timestamp'] - this.offset,
+                        y: sburndata[i]['attributes']['value']
+                    });
+                }
+                // guaranteed not to have more than maxPoints in the incoming data\
+                // so no need to remove extras
             }
         }
     };
+
+
+    this.updateHeater = function (adata) {
+        this.updateData(adata);
+        this.draw();
+        setTimeout(() => __heaterCallback(this, this.startUpdateHeater), this.period);
+    };
+
+    this.startUpdateHeater = function () {
+        let sts = new Date(this.lastLoaded);
+        let ts = new Date();
+        this.lastLoaded = ts;
+        $.getJSON(this.url,
+            {'starttime': sts.toISOString(), 'endtime': ts.toISOString(), 'datapts': this.maxPoints},
+            data => __heaterDataCallback(data, this, this.updateHeater)
+        );
+    };
+
+    this.setupHeater = function () {
+        let sts = new Date();
+        let ts = new Date(sts);
+        sts.setHours(sts.getHours() - 24);
+        this.lastLoaded = ts;
+        $.getJSON(this.url,
+            {'starttime': sts.toISOString(), 'endtime': ts.toISOString(), 'datapts': this.maxPoints },
+            data => __heaterDataCallback(data, this, this.updateHeater)
+            );
+    };
+
     this.draw = function () {
         this.gaugeIn.draw();
         this.gaugeOut.draw();
