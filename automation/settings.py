929a9ee2fc198a41ea5d96487d6f1fb5d4b407ee
Jim Nusbaum <raymond.nusbaum@getyourvi.com>
2021-04-07 15:28:18 -0600

    refactor Django apps

--- automation/settings.py
+++ automation/settings.py
@@ -172,18 +172,18 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'static')
 
 # app specific settings - should they be here?
 
-DATASERVER_HOST = 'http://192.168.0.134/automation'
-MQTTHOST = '192.168.0.134'
-TEMPMQTTID = 'tempcapture'
-RELAYMQTTID = 'relaycapture'
-DEVCFGMQTTID = 'deviceconfig'
-DEVSTATUSMQTTID = 'devstatuscapture'
-BASETOPIC = 'sorrelhills'
-
-# DATASERVER_HOST = 'http://127.0.0.1:8000/automation'
+# DATASERVER_HOST = 'http://192.168.0.134/automation'
 # MQTTHOST = '192.168.0.134'
-# TEMPMQTTID = 'testtempcapture'
-# RELAYMQTTID = 'testrelaycapture'
-# DEVCFGMQTTID = 'testdeviceconfig'
-# DEVSTATUSMQTTID = 'testdevstatuscapture'
+# TEMPMQTTID = 'tempcapture'
+# RELAYMQTTID = 'relaycapture'
+# DEVCFGMQTTID = 'deviceconfig'
+# DEVSTATUSMQTTID = 'devstatuscapture'
 # BASETOPIC = 'sorrelhills'
+
+DATASERVER_HOST = 'http://127.0.0.1:8000/automation'
+MQTTHOST = '192.168.0.134'
+TEMPMQTTID = 'testtempcapture'
+RELAYMQTTID = 'testrelaycapture'
+DEVCFGMQTTID = 'testdeviceconfig'
+DEVSTATUSMQTTID = 'testdevstatuscapture'
+BASETOPIC = 'sorrelhills'
