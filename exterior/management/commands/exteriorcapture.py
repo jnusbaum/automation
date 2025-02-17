691bff9f37d29b5d07edd1c8ea4a8ee4debc51dd
Jim Nusbaum <jnusbaum@supraluminalsoftware.com>
2025-02-16 18:03:50 -0700

    checkpoint env capture

--- exterior/management/commands/exteriorcapture.py
+++ exterior/management/commands/exteriorcapture.py
@@ -1,6 +1,6 @@
 from ambient_api.ambientapi import AmbientAPI
 from django.core.management.base import BaseCommand
-import time
+import datetime, time
 import logging
 
 
@@ -16,4 +16,8 @@ class Command(BaseCommand):
         devices = api.get_devices()
         device = devices[0]
         time.sleep(1)  # pause for a second to avoid API limits
-        print(device.get_data())
+        ddata = device.get_data()
+        print(datetime.datetime.fromtimestamp(ddata[0]['dateutc']/1000).strftime('%Y-%m-%d %H:%M:%S'))
+        print(f"outside temp = {ddata[0]['tempf']}")
+        print(f"inside temp = {ddata[0]['tempinf']}")
+        print(f"sunshine = {ddata[0]['solarradiation']}")
